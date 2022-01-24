67f311129e04eba92ac197e68ad5b91cd8534882
Om Nankar <53141204+omnankar10@users.noreply.github.com>
2022-01-25 00:23:51 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,36 +1,36 @@
 var defaultThreads = [{
         id: 1,
-        title: "Thread 1",
+        title: "Forum",
         author: "Aaron",
         date: Date.now(),
         content: "Thread content",
         comments: [{
-                author: "Jack",
+                author: "XYZ",
                 date: Date.now(),
-                content: "Hey there"
+                content: "Hey, I wanted to know about 3rd dose vaccine procedure."
             },
             {
-                author: "Arthur",
+                author: "ABC",
                 date: Date.now(),
-                content: "Hey to you too"
+                content: "Hey to you too, kindly visit www.cowin.gov.in"
             }
         ]
     },
     {
         id: 2,
-        title: "Thread 2",
+        title: "Forum 2",
         author: "Aaron",
         date: Date.now(),
         content: "Thread content 2",
         comments: [{
-                author: "Jack",
+                author: "PQR",
                 date: Date.now(),
-                content: "Hey there"
+                content: "Hello, I wanted to know about the omicron symptoms?"
             },
             {
-                author: "Arthur",
+                author: "LMN",
                 date: Date.now(),
-                content: "Hey to you too"
+                content: "Actually, most of the symptoms are just body pain and fever over cold and cough. Be safe."
             }
         ]
     }
